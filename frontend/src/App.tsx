2af5e154fa8d23d78305832b76c3dd12b74eee19
James O'Kane <orbiccode@gmail.com>
2025-11-20 14:01:38 +0000

    created translation form shell

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css';
 import Header from './components/Header/Header';
+import TranslationForm from './components/TranslationForm/TranslationForm';
 
 function App() {
   return (
     <main className='mainContainer'>
       <Header />
       <section className='translationSection'>
-        <div className='translationContainer'></div>
+        <div className='translationContainer'>
+          <TranslationForm />
+        </div>
       </section>
     </main>
   );
---
+++ frontend/src/components/TranslationForm/TranslationForm.tsx
@@ -0,0 +1,7 @@
+export default function TranslationForm() {
+  return (
+    <form>
+      <h2>Form</h2>
+    </form>
+  );
+}
