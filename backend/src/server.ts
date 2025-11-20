5df6f9f452e734180ff4b1256848778ffc0fd62c
James O'Kane <orbiccode@gmail.com>
2025-11-20 09:36:23 +0000

    added 404 catch

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import type { Request, Response } from 'express';
+import { translationRouter } from './routes/translation';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response<{ message: string }>): void => {
-  res.json({ message: 'hello' });
+app.use('/translation', translationRouter);
+
+app.use((req: Request, res: Response<{ message: string }>): void => {
+  res.status(404).json({ message: 'Endpoint not found' });
 });
 
 app.listen(PORT, () => console.log('server is listening on port: ', PORT));
