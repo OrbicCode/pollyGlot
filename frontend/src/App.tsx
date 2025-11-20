34d5486346a3dc23ced242c270d19eaf7884090d
James O'Kane <orbiccode@gmail.com>
2025-11-20 15:46:10 +0000

    form now passes up data onSubmit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,22 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import TranslationForm from './components/TranslationForm/TranslationForm';
 
+export interface Data {
+  text: string;
+  selectedLanguage: string;
+}
+
 function App() {
+  const [data, setData] = useState<Data>();
+  console.log(data);
   return (
     <main className='mainContainer'>
       <Header />
       <section className='translationSection'>
         <div className='translationContainer'>
-          <TranslationForm />
+          <TranslationForm setData={setData} />
         </div>
       </section>
     </main>
--- frontend/src/components/TranslationForm/TranslationForm.tsx
+++ frontend/src/components/TranslationForm/TranslationForm.tsx
@@ -1,7 +1,89 @@
-export default function TranslationForm() {
+import { useState } from 'react';
+import type { JSX } from 'react';
+import styles from './TranslationForm.module.css';
+import frFlag from '../../assets/fr-flag.png';
+import spFlag from '../../assets/sp-flag.png';
+import jpFlag from '../../assets/jpn-flag.png';
+import type { Data } from '../../App';
+
+interface TranslationFormProps {
+  setData: (data: Data) => void;
+}
+
+export default function TranslationForm({ setData }: TranslationFormProps): JSX.Element {
+  const [text, setText] = useState<string>('');
+  const [selectedLanguage, setSelectedLanguage] = useState<string>('french');
+
+  function handleTextChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    setText(e.target.value);
+  }
+
+  function handleRadioChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setSelectedLanguage(e.target.value);
+  }
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    setData({
+      text,
+      selectedLanguage,
+    });
+  }
+
   return (
-    <form>
-      <h2>Form</h2>
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <label htmlFor='text' className={styles.sectionHeader}>
+        Text to translate 👇
+      </label>
+      <textarea
+        id='text'
+        value={text}
+        className={styles.textarea}
+        onChange={handleTextChange}
+      ></textarea>
+      <fieldset className={styles.radioFieldset}>
+        <legend className={styles.sectionHeader}>Select language 👇</legend>
+        <ul>
+          <li>
+            <input
+              name='language'
+              value='french'
+              id='french'
+              type='radio'
+              checked={selectedLanguage === 'french'}
+              onChange={(e) => handleRadioChange(e)}
+            />
+            <label>French</label>
+            <img src={frFlag} alt='The French flag.' width={30} height={20} />
+          </li>
+          <li>
+            <input
+              name='language'
+              value='spanish'
+              id='spanish'
+              type='radio'
+              checked={selectedLanguage === 'spanish'}
+              onChange={(e) => handleRadioChange(e)}
+            />
+            <label>Spanish</label>
+            <img src={spFlag} alt='The Spanish flag.' width={30} height={20} />
+          </li>
+          <li>
+            <input
+              name='language'
+              value='japanese'
+              id='japanese'
+              type='radio'
+              checked={selectedLanguage === 'japanese'}
+              onChange={(e) => handleRadioChange(e)}
+            />
+            <label>Japanese</label>
+            <img src={jpFlag} alt='The Japanese flag.' width={30} height={20} />
+          </li>
+        </ul>
+      </fieldset>
+      <button>Translate</button>
     </form>
   );
 }
